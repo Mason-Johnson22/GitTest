f348fd2e9eaca0a82338f98c12e7a9f99f23b91c
Riley Hill <059667@swcsd.us>
2019-10-24 12:35:25 -0400

    please help me my mom left me at best buy

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //Bruh moment fortnite burger aaaaaaaaaaa
+        //Help me
     }
 
 
