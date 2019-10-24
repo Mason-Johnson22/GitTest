c00f75d3f60744e624fe82f9a7ad053bd234bd70
Mason Johnson <masonalexanderjohnson@masons-macbook-air.local>
2019-10-24 12:33:05 -0400

    aaaaaaaa

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Bruh moment fortnite burger
+        //Bruh moment fortnite burger aaaaaaaaaaa
     }
 
 
